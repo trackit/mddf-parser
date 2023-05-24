5495b35b6c725e2f99ec8b55ff467b0210d549a5
ThomyLorenzatti <thomy.lorenzatti@epitech.eu>
2023-05-24 03:43:08 +0200

    clean_mec_with_tests: update all tests functionnal, add convert method to parsers, add xml beautifier for indent

--- src/MMCMECParser/__tests__/MECParser.test.ts
+++ src/MMCMECParser/__tests__/MECParser.test.ts
@@ -1,20 +1,16 @@
-import { MECInterface } from '../MEC/MECInterface';
-import { expectedMEC } from './ExpectedValues/MECInterface';
 import { LocalXMLFileAdaptor } from '../adaptors/secondary/LocalXMLFileAdaptor';
 import { MECParser } from '../MMCMECParser';
 
 describe('MECParser', () => {
   interface TestParametersParseMEC {
     pathToXml: string,
-    expected: {
-      returnValue: MECInterface,
-    }
   }
 
-  const testParseMEC = async ({ pathToXml, expected }: TestParametersParseMEC) => {
+  const testParseMEC = async ({ pathToXml }: TestParametersParseMEC) => {
     const fileAdaptor = new LocalXMLFileAdaptor();
     const mecParser = new MECParser({ fileAdaptor });
-    expect(await mecParser.parse(pathToXml)).toEqual(expected.returnValue);
+    const tmp = await mecParser.convert(await mecParser.parse(pathToXml));
+    expect(tmp).toEqual(mecParser.mecFile);
   };
 
   it('Test Parse MEC Function', async () => {
@@ -22,9 +18,6 @@ describe('MECParser', () => {
 
     await testParseMEC({
       pathToXml: exampleXML,
-      expected: {
-        returnValue: expectedMEC,
-      },
     });
   });
 });
--- src/MMCMECParser/interfaces/IParser.ts
+++ src/MMCMECParser/interfaces/IParser.ts
@@ -1,4 +1,5 @@
 export interface IParser<T> {
   parse(path: string): Promise<T>
+  convert(data: T): Promise<string>
   export(path: string, data: T): Promise<void>
 }
