c8f06eb372edb1ad0392403214af49d79aa25472
gauti <gautier.blandin@gmail.com>
2023-06-25 23:36:08 +0200

    Add tentative work on SchemaConformer

--- src/SchemaConformer/SchemaConformer.test.ts
+++ src/SchemaConformer/SchemaConformer.test.ts
@@ -6,17 +6,19 @@ import { LocalFileAdaptor } from '../FileAdaptor/LocalFileAdaptor';
 describe('SchemaConformer', () => {
   const charKey = 'Value';
 
-  const rawObject = generateSingleElementArrayRawJSON(charKey);
-  const expectedObject = generateSingleElementArrayJSON(charKey);
+  describe('conform', () => {
+    const rawObject = generateSingleElementArrayRawJSON(charKey);
+    const expectedObject = generateSingleElementArrayJSON(charKey);
 
-  it('should conform the object to the schema', async () => {
-    const fileReader = new LocalFileAdaptor();
-    const schema = JSON.parse(await fileReader.readFile('assets/JSONSchemaSamples/simple_array.json'));
+    it('should conform the object to the schema', async () => {
+      const fileReader = new LocalFileAdaptor();
+      const schema = JSON.parse(await fileReader.readFile('assets/JSONSchemaSamples/simple_array.json'));
 
-    const conformer = new SchemaConformer(schema);
+      const conformer = new SchemaConformer(schema);
 
-    console.log(JSON.stringify(conformer.getSchema(), null, 2));
+      console.log(JSON.stringify(conformer.getSchema(), null, 2));
 
-    expect(conformer.conform(rawObject)).toEqual(expectedObject);
+      expect(conformer.conform(rawObject)).toEqual(expectedObject);
+    });
   });
 });
