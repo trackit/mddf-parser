8cef9524f2245d8b5b72884ab10fe3603e65d0e5
gauti <gautier.blandin@gmail.com>
2023-06-25 20:42:19 +0200

    Add number parsing in XMLRawParser

--- assets/JSONSamples/elements_array_with_different_tags.ts
+++ assets/JSONSamples/elements_array_with_different_tags.ts
@@ -5,12 +5,12 @@ export default function generateElementsArrayWithDifferentTagsJSON(charKey: stri
         {
           item: { [charKey]: 'Sample Item 1' },
           description: { [charKey]: 'Description for Item 1' },
-          price: { [charKey]: '15.99' },
+          price: { [charKey]: 15.99 },
         },
         {
           item: { [charKey]: 'Sample Item 2' },
           description: { [charKey]: 'Description for Item 2' },
-          price: { [charKey]: '20.99' },
+          price: { [charKey]: 20.99 },
         },
       ],
     },
--- assets/JSONSamples/many_attribute_on_same_tag.ts
+++ assets/JSONSamples/many_attribute_on_same_tag.ts
@@ -5,12 +5,12 @@ export default function generateManyAttributeOnSameTagJSON(): object {
         {
           name: 'Sample Item 1',
           description: 'This is a sample item',
-          price: '15.99',
+          price: 15.99,
         },
         {
           name: 'Sample Item 2',
           description: 'This is another sample item',
-          price: '20.99',
+          price: 20.99,
         },
       ],
     },
