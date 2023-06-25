e6974849573b8f27eb527c3eea9c8840a88336b3
gauti <gautier.blandin@gmail.com>
2023-06-25 20:25:06 +0200

    Fix double quote

--- assets/JSONSamples/nested_arrays_and_objets.ts
+++ assets/JSONSamples/nested_arrays_and_objets.ts
@@ -1,32 +1,32 @@
 export default function generateComplexObjectJSON(charKey: string) {
   return {
     company: {
-    name: { [charKey]: "My Company" },
+      name: { [charKey]: 'My Company' },
       address: {
-        street: { [charKey]: "Main St. 123" },
-        city: { [charKey]: "Big City" },
-        country: { [charKey]: "Country" },
+        street: { [charKey]: 'Main St. 123' },
+        city: { [charKey]: 'Big City' },
+        country: { [charKey]: 'Country' },
       },
       employees: {
         employee: [
           {
-            name: { [charKey]: "John Doe" },
-            role: { [charKey]: "Developer" },
+            name: { [charKey]: 'John Doe' },
+            role: { [charKey]: 'Developer' },
             skills: {
               skill: [
-                { [charKey]: "JavaScript" },
-                { [charKey]: "Typescript" },
-                { [charKey]: "React" },
+                { [charKey]: 'JavaScript' },
+                { [charKey]: 'Typescript' },
+                { [charKey]: 'React' },
               ],
             },
           },
           {
-            name: { [charKey]: "Jane Doe" },
-            role: { [charKey]: "Product Manager" },
+            name: { [charKey]: 'Jane Doe' },
+            role: { [charKey]: 'Product Manager' },
             skills: {
               skill: [
-                { [charKey]: "Scrum" },
-                { [charKey]: "Kanban" },
+                { [charKey]: 'Scrum' },
+                { [charKey]: 'Kanban' },
               ],
             },
           },
