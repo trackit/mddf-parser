79ff73a37d4d5d37088bbf4890a24017ee81744b
gauti <gautier.blandin@gmail.com>
2023-06-25 18:17:31 +0200

    Remove single_element_array.ts

--- assets/JSONSamples/single_element_array.ts
+++
@@ -1,9 +0,0 @@
-export default function generateSingleElementArrayJSON(charKey: string): object {
-  return {
-    sample: {
-      items: {
-        item: { [charKey]: 'Sample Item 1' },
-      },
-    },
-  };
-}
